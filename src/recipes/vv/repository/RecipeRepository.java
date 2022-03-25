d7e017ec50f69b6b67f116328162ef530686d8e0
DESKTOP-063BMPJ\Vinny <vvauban@gmail.com>
2022-03-25 21:22:00 +0100

    init

---
+++ src/recipes/vv/repository/RecipeRepository.java
@@ -0,0 +1,17 @@
+package recipes.vv.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import recipes.vv.model.VVRecipe;
+
+import java.util.List;
+
+
+public interface RecipeRepository extends JpaRepository<VVRecipe, Integer> {
+    @Query(value="SELECT * FROM VVRECIPE r WHERE LOWER(r.NAME) LIKE LOWER(concat('%', :name,'%')) ORDER BY r.date DESC",nativeQuery = true)
+    List<VVRecipe> findByName(@Param("name")String name);
+
+    @Query(value="SELECT * FROM VVRECIPE r WHERE LOWER(r.CATEGORY) LIKE LOWER(:category) ORDER BY r.date DESC",nativeQuery = true)
+    List<VVRecipe> findByCategory(@Param("category")String category);
+}
